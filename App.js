3318e5a0446b437d753fba68187c20814c6dd97d
Greg Lim <limjunqu@gmail.com>
2020-06-22 15:13:34 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'native-base';
+import GitHub from './GitHub';
+
+export default function App() {
+  
+  return (
+    <Container>
+      <GitHub />
+    </Container>
+  );
+}
