34c8aa650b0443f39dcd894f3ba6fba1ce2aa3ca
Greg Lim <limjunqu@gmail.com>
2020-06-23 11:16:53 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import { Container } from 'native-base';
-import GitHub from './GitHub';
+import React, { useReducer } from 'react';
+import ToDoList from './ToDoList';
+
+const todosInitialState = { 
+  todos:[{ id:'1', text: "finishing writing hooks chapter"},
+    { id:'2', text: "play with kids"},
+    { id:'3', text: "read bible"}
+  ]
+};
+
+export const TodosContext = React.createContext()
 
 export default function App() {
-  
+
+  const [state, dispatch] = useReducer(todosReducer,todosInitialState)
+
   return (
-    <Container>
-      <GitHub />
-    </Container>
-  );
+    <TodosContext.Provider value={{state,dispatch}}>      
+      <ToDoList />
+    </TodosContext.Provider>    
+  )
+}
+
+function todosReducer(state, action){ 
+  switch(action.type){ 
+    case 'add':      
+      // add new todo onto array
+      const addedToDos = [...state.todos,action.payload]
+      // spread our state and assign todos
+      return {...state,todos:addedToDos} 
+    case 'edit':   
+      const updatedToDo = {...action.payload} 
+      const updatedToDoIndex = state.todos.findIndex(t => t.id === action.payload.id)
+      const updatedToDos = [
+        ...state.todos.slice(0,updatedToDoIndex),
+        updatedToDo,
+        ...state.todos.slice(updatedToDoIndex + 1)
+      ];
+      return {...state, todos: updatedToDos}      
+    case 'delete':
+      const filteredTodoState = state.todos.filter( todo => todo.id !== action.payload.id)
+      return {...state, todos: filteredTodoState}
+    default:
+      return todosInitialState
+  }
 }
